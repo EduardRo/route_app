3943b101f3a832eae24d2ba245b4b58676b4a947
EduardRo <eduardstancu@yahoo.com>
2021-06-26 12:52:23 +0300

    sec commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    title: 'Named Routes',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => FirstScreen(),
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/second');
+            },
+            child: Text('Lauch screen')),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text('Go back!'),
+      )),
+    );
+  }
+}
